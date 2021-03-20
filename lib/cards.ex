e22418b90dee4a1f211dab5f08a632a2d74ea3c2
Johnkinyanjui <johnkinyanjuiwanjiru@gmail.com>
2021-03-20 21:59:25 +0300

    first commit

---
+++ lib/cards.ex
@@ -0,0 +1,44 @@
+#iex -S mix
+
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    cards = for value <-values, suit <- suits do
+      "#{value} of #{suit}"
+    end
+    List.flatten(cards)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+       {:ok, binary} -> :erlang.binary_to_term binary
+       {:error, _reason} -> "File does not exists"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |>Cards.shuffle
+    |>Cards.deal(hand_size)
+  end
+
+end
